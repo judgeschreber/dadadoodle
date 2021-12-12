4e4f00d55e98305c988328960980141013caf0a5
Lukas Frenzer <lukas.frenzer@web.de>
2021-12-13 00:30:54 +0100

    doodle redirect and handlebars update

--- public/sketch.js
+++ public/sketch.js
@@ -1,37 +1,106 @@
 let colorButton = document.getElementsByClassName("color-button");
+let lineButton = document.getElementsByClassName("line-button");
 
 console.log("button: ", colorButton);
 
-let color = "white";
+let strokeColor = "black";
+let sW = 2;
+let dotsArray;
+let newDotsArray;
 
 colorButton.forEach((element) => {
     element.addEventListener("click", function (event) {
         console.log("click", event.target.id);
-        color = event.target.id;
+        strokeColor = event.target.id;
     });
+});
+
+console.log("lineButton: ", lineButton);
+lineButton[0].addEventListener("click", function (event) {
+    if (sW < 11) {
+        sW++;
+        event.target.style.width = `${sW}px`;
+        console.log("width: ", event.target.style.width);
+    } else {
+        sW = 1;
+    }
 });
 
 function setup() {
     createCanvas(600, 400);
-    background(51);
+
+    noFill();
+    dotsArray = [];
+    newDotsArray = [];
 
     socket = io.connect("http://localhost:3000");
     socket.on("mouse", newDoodle);
+    socket.on("mouseoff", otherMouseReleased);
 }
 
 function newDoodle(data) {
-    noStroke();
-    fill(color);
-    ellipse(data.x, data.y, 36, 36);
+    stroke(data.strokeColor);
+    strokeWeight(data.strokeWeight);
+    beginShape();
+
+    // declare the points as an array
+
+    newDotsArray.forEach((element) => {
+        curveVertex(element.x, element.y);
+    });
+
+    // create an object as empty point
+    var dot = {};
+    dot.x = data.x;
+    dot.y = data.y;
+
+    // add the point to the array
+    newDotsArray.push(dot);
+    endShape();
 }
 
-function draw() {
+function draw() {}
+
+function mouseDragged() {
+    console.log("points before: ", dotsArray);
+
+    console.log("mouse drag!");
     let data = {
         x: mouseX,
         y: mouseY,
+        strokeColor: strokeColor,
+        strokeWeight: sW,
     };
     socket.emit("mouse", data);
-    noStroke();
-    fill(color);
-    ellipse(mouseX, mouseY, 36, 36);
+    strokeWeight(sW);
+    stroke(strokeColor);
+    beginShape();
+
+    // declare the points as an array
+
+    dotsArray.forEach((element) => {
+        curveVertex(element.x, element.y);
+    });
+
+    var dot = {};
+    dot.x = mouseX;
+    dot.y = mouseY;
+
+    dotsArray.push(dot);
+    endShape();
+}
+
+function mouseReleased() {
+    dotsArray = [];
+    let data = {
+        mouseoff: true,
+    };
+    socket.emit("mouseoff", data);
+}
+
+function otherMouseReleased(data) {
+    console.log("otherMouseReleased");
+    if (data.mouseoff == true) {
+        newDotsArray = [];
+    }
 }
--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 const express = require("express");
 const app = express();
-
 const socket = require("socket.io");
-
 app.use(express.static("./public"));
 
+const { v4: uuidv4 } = require("uuid");
+
+const { engine } = require("express-handlebars");
+app.engine(
+    "handlebars",
+    engine({ extname: ".handlebars", defaultLayout: "main" })
+);
+
+app.set("view engine", "handlebars");
+
 console.log("hello!!");
+
+app.get("/", (req, res) => {
+    let randomUrl = uuidv4();
+    console.log("randomUrl ", randomUrl);
+    res.render("start", {
+        url: randomUrl,
+    });
+});
+
+app.get("/doodle/*", (req, res) => {
+    res.render("doodle");
+});
 
 const server = app.listen(3000);
 
@@ -15,8 +35,11 @@ io.sockets.on("connection", newConnection);
 function newConnection(socket) {
     console.log("new connection: ", socket.id);
     socket.on("mouse", mouseEmit);
+    socket.on("mouseoff", mouseEmit);
 
     function mouseEmit(data) {
+        console.log("mouseEmit triggered");
         socket.broadcast.emit("mouse", data);
+        socket.broadcast.emit("mouseoff", data);
     }
 }
